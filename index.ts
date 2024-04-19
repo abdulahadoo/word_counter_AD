31868bbebe40c7eba1186b0c5ba2ded36f406059
Ab ahad <164404456+abdulahadoo@users.noreply.github.com>
2024-04-19 16:33:05 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+//this line called a shebang, it tell us the OS to run with jode js
+
+import inquirer from "inquirer"
+
+//Declare a constant 'answere' and assign it to the result of inquirer.prompt function
+const answere :{
+    sentence: string
+
+} = await inquirer.prompt([
+        {
+           name:"sentence",
+           type:"input",
+           message:"Enter your sentence to count the word:"
+        }
+    ])
+
+    const words = answere.sentence.trim().split(" ")
+
+    // Print the array of words to the console
+    console.log(words)
+
+   // Print the word count of the sentence to the console
+
+    console.log(`Your sentence word count is ${words.length}`);
+
+    
